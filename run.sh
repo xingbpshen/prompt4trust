f4cb9c743f51074a0f999fe02b69c75f6a29120c
Xing Shen <xing.shen@mail.mcgill.ca>
2025-07-12 09:03:44 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@
 #SBATCH --gres=gpu:a100l:4
 #SBATCH --cpus-per-task=6 #
 #SBATCH --mem=128G 
-#SBATCH -o /network/scratch/a/anita.kriz/slurm-%j.out  # Write the log on scratch
+#SBATCH -o XXX/slurm-%j.out  # Write the log on scratch
 
 # 1. Load the required modules
 module load python/3.10
 
 # 2. Activate the venv10 environment
-source /network/scratch/a/anita.kriz/venvs/vccrl_env/bin/activate
+source XXX/bin/activate
 
 # 3. Run your script with the same command you'd use interactively
 python main.py --config pmcvqa.yml --log_folder test --trial_name test_1 --train --ni 
